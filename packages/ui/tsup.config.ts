9b177c399a378d3c99c0309b1c180c4b94278f54
SonMooSans <god63820869@gmail.com>
2023-11-17 00:04:59 +0800

    UI: Build typescript declaration files

--- packages/ui/tsup.config.ts
+++ packages/ui/tsup.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "tsup";
 const config = defineConfig({
   entry: ["./src/index.tsx"],
   format: "esm",
+  dts: true,
   banner: () => ({ js: "'use client';" }),
   outExtension: () => ({ js: ".js" }),
 });
