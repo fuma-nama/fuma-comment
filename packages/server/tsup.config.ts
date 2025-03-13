19dfe7722e4bc5c0756bceb72e4cd7d3e7858c4f
Fuma Nama <god63820869@gmail.com>
2025-03-13 22:33:43 +0800

    docs: add docs for Hono.js

--- packages/server/tsup.config.ts
+++ packages/server/tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "tsup";
 export default defineConfig({
 	entry: [
 		"./src/index.ts",
-		"./src/{express,custom,elysia,next}/index.ts",
+		"./src/{express,custom,elysia,next,hono}/index.ts",
 		"./src/adapters/*.ts",
 	],
 	dts: true,
